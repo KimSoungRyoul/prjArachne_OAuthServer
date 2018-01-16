8b273b73ea0e9ab7d55019732e6a3b06214b9564
KimSoungRyoul <KimSoungRyoul@gmail.com>
2018-01-17 01:32:02 +0900

    first commit ksr OAuth server

---
+++ SmartMirrorOAuthServerProject_arachne/src/main/java/org/prj/arachne/SmartMirrorOAuthServerProjectArachneApplication.java
@@ -0,0 +1,12 @@
+package org.prj.arachne;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SmartMirrorOAuthServerProjectArachneApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SmartMirrorOAuthServerProjectArachneApplication.class, args);
+	}
+}
